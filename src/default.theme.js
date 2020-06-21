57acfedadfbae4f15797f5afbbc43cfb95ad105d
iMplode nZ <rouli.freeman@gmail.com>
2020-06-20 17:05:52 -0700

    Made the thing activatable.

--- src/default.theme.js
+++ src/default.theme.js
@@ -4,7 +4,15 @@ export default (epp, theme) => {
     import('./index.styl');
     const splitElements = 'Editor++'.split('').flatMap((e) => [$.span([]), e]);
     const titleInterior = $.div('title-interior', splitElements);
-    const title = $.div('title', [$.div.icon('icon', []), titleInterior]);
+    const title = $.div(
+        'title',
+        {
+            onclick() {
+                title.classList.toggle('activated');
+            },
+        },
+        [$.div.icon('icon', []), titleInterior]
+    );
     const interior = $.div('interior', []);
     const root = $.div('container', [title, interior]);
     document.body.appendChild(root);
