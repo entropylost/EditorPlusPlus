a071e4d5d7be008d0800ed65b45594fa3ba62d5f
iMplode nZ <rouli.freeman@gmail.com>
2020-06-20 19:41:36 -0700

    Shifted places, made private and changed colors.

--- src/default.theme.js
+++ src/default.theme.js
@@ -2,6 +2,7 @@ import $ from '@implode-nz/html/';
 
 export default (epp, theme) => {
     import('./index.styl');
+    import('./epp.svg').then((svg) => document.querySelectorAll('.icon').forEach((x) => (x.innerHTML = svg.default)));
     const splitElements = 'Editor++'.split('').flatMap((e) => [$.span([]), e]);
     const titleInterior = $.div('title-interior', splitElements);
     const title = $.div(
